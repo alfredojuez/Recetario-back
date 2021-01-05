f1de51098e8ea55740432d6fe6a65439da6f45aa
Alfredo Juez <Alfredojuez@hotmail.com>
2021-01-06 00:42:21 +0100

    Ya tenemos la insercción de usuarios mediante GraphQL
    Para ello en la sección Query escribimos:
    mutation addUser($user: UserInput!)
    {
      register(User: $user)
      {
        id
        email
        nombre
        apellidos
        usuario
        pass
        foto
        nacionalidad
        perfil
        alta
        ultimoLogin
        activo
      }
    }
    y en la sección Variables esto:
    {
    	"user":{
        "email":"demo2@hotmail.com",
        "nombre": "demo2",
        "apellidos":"demo2" ,
        "usuario": "demo",
        "pass": "",
        "foto": "",
        "nacionalidad": "",
        "perfil": "USER"
      }
    }
    Ejecutamos GraphQL y debería introducir el nuevo usuario sin problemas

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,18 +1,12 @@
 import chalk from "chalk";
 import MongoClient from "mongodb";
 
-enum colores
-{
-  KO = 'red',
-  OK = 'green',
-};
-
 class Database {
 
   async init() {
 
     //leemos los datos de las variables de entorno
-    const MONGO_DB = process.env.DATABASE || "mongodb://localhost:27017/recetario";
+    const MONGO_DB = process.env.DATABASE || "mongodb://localhost:27017";
 
     //Configuracmos el cliente
     const client = await MongoClient.connect(MONGO_DB, {
@@ -30,6 +24,7 @@ class Database {
       console.log(`NOMBRE: ` + chalk.green(db.databaseName));
       console.log("===========================================");
     }
+    return db;  
   }
 }
 
